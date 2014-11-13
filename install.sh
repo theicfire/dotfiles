8869e29cede06b9c9f79da94af5022bff879229f
Chase Lambert <theicfire@gmail.com>
2014-11-13 11:33:06 -0500

    Reordering linking in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 cd ~
+ln -s ~/.config/dotfiles/.bash_aliases .bash_aliases
+ln -s ~/.config/dotfiles/.bashrc .bashrc
+ln -s ~/.config/dotfiles/.fzf.bash .fzf.bash
+ln -s ~/.config/dotfiles/.gitconfig .gitconfig
+ln -s ~/.config/dotfiles/.gitconfig_global .gitconfig_global
+ln -s ~/.config/dotfiles/.inputrc .inputrc
+ln -s ~/.config/dotfiles/.profile .profile
+ln -s ~/.config/dotfiles/.tmux.conf .tmux.conf
 ln -s ~/.config/dotfiles/vim .vim
 ln -s ~/.config/dotfiles/vim/.vimrc .vimrc
-ln -s ~/.config/dotfiles/.bashrc .bashrc
-ln -s ~/.config/dotfiles/.profile .profile
-ln -s ~/.config/dotfiles/.bashrc .bashrc
-ln -s ~/.config/dotfiles/.bash_aliases .bash_aliases
-ln -s ~/.config/dotfiles/.gitconfig .gitconfig
-
